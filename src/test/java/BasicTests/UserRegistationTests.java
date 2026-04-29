08ecf5b7535c79ff29e82faee986b06d87f42fde
Sherley <makofane.st@gmail.com>
2026-04-29 22:04:58 +0200

    first commit

---
+++ src/test/java/BasicTests/UserRegistationTests.java
@@ -0,0 +1,93 @@
+package BasicTests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserRegistationTests {
+
+    String BaseURL = "https://ndosiautomation.co.za/APIDEV";
+    String registeredUserId;
+    String adminToken;
+
+    @Test (priority = 1)
+    public void userRegistrationTest() {
+
+        String registerUserPath = "/register";
+
+        String userRegistrationPayload = "{\n" +
+                "  \"firstName\": \"Siba\",\n" +
+                "  \"lastName\": \"Testing\",\n" +
+                "  \"email\": \"Group01Test@test.com\",\n" +
+                "  \"password\": \"1234567!\",\n" +
+                "  \"confirmPassword\": \"1234567!\",\n" +
+        "  \"groupId\": \"c1ce77c4-bd1a-42ae-901f-fc3e534c55b8\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .baseUri(BaseURL)
+                .basePath(registerUserPath)
+                .header("Content-Type", "application/json")
+                .body(userRegistrationPayload)
+                .log().all()
+                .post()
+                .then().extract().response();
+
+        int responseStatusCode = response.getStatusCode();
+        System.out.println("Status Code: " + responseStatusCode);
+        System.out.println(("Response Body: " + response.getBody().asString()));
+        Assert.assertEquals(responseStatusCode, 201, "Expected status code 200");
+
+        registeredUserId = response.jsonPath().getString("data.id");
+        // Code for user registration test
+    }
+
+    @Test (priority = 2)
+    public void adminLoginTest() {
+
+        String loginPath = "/login";
+        String adminLoginPayload = "{\n" +
+                "  \"email\": \"admin@gmail.com\",\n" +
+                "  \"password\": \"@12345678\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .baseUri(BaseURL)
+                .basePath(loginPath)
+                .header("Content-Type", "application/json")
+                .body(adminLoginPayload)
+                .log().all()
+                .post()
+                .then().extract().response();
+
+        int responseStatusCode = response.getStatusCode();
+        System.out.println("Status Code: " + responseStatusCode);
+        System.out.println(("Response Body: " + response.getBody().asString()));
+        Assert.assertEquals(responseStatusCode, 200, "Expected status code 200");
+
+        adminToken = response.jsonPath().getString("data.token");
+        // Code for admin login test
+    }
+
+    @Test(priority = 3)
+    public void approveUser(){
+
+        String approveUserPath = "/admin/users/"+registeredUserId+"/approve";
+
+        Response response = RestAssured.given()
+                .baseUri(BaseURL)
+                .basePath(approveUserPath)
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .log().all()
+                .put()
+                .then().extract().response();
+
+        System.out.println(("Response Body: " + response.getBody().asString()));
+
+    }
+
+}
+
+
